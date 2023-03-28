bd3843e6c885b1f8b85361d560e2fca4a5943ba7
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-28 21:04:25 +0300

    1

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -4,8 +4,10 @@ import {
   Text,
   StyleSheet,
   ImageBackground,
-  TextInput,
+    TextInput,
+  TouchableOpacity,
 } from "react-native";
+
 
 const RegistrationScreen = () => {
   return (
@@ -14,8 +16,29 @@ const RegistrationScreen = () => {
         style={styles.bgImage}
         source={require("../assets/image/PhotoBG.png")}
       >
-        <View>
-          <TextInput style={styles.input} placeholder="Login" />
+        <View style={styles.formWrap}>
+          <View style={styles.form}>
+            <Text style={styles.inputTitle}>Registration</Text>
+            <View>
+              <TextInput style={styles.input} placeholder="Login" />
+            </View>
+            <View style={{ marginTop: 16 }}>
+              <TextInput style={styles.input} placeholder="Email" />
+            </View>
+            <View style={{ marginTop: 16 }}>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Password"
+              />
+            </View>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.buttonContainer}
+            >
+                <Text style={styles.buttonText}>SIGN IN</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ImageBackground>
     </View>
@@ -43,9 +66,45 @@ const styles = StyleSheet.create({
     height: 50,
     width: 400,
     padding: 16,
-    marginHorizontal: 40,
     marginVertical: 10,
     fontSize: 16,
     textAlign: "center",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  inputTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    lineHeight: 40,
+    color: "#212121",
+    marginBottom: 16,
+  },
+  formWrap: {},
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 16,
+    gap: 16,
+    marginTop: 43,
+    borderRadius: 30,
+    overflow: "hidden",
+    height: 50,
+    width: "100%",
+    marginBottom: 15,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  gradient: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    height: 55,
   },
 });
