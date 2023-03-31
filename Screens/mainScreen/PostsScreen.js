ea187fb5f6f4786a99d08804feb6e87a076399c4
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-31 17:18:37 +0300

    started

--- Screens/mainScreen/PostsScreen.js
+++ Screens/mainScreen/PostsScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-const PostsScreen = () => {
+
+
+const PostsScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <MaterialIcons
+          name="logout"
+          size={24}
+          color="black"  
+          />
+        </View>
       <Text>PostsScreen</Text>
     </View>
   );
@@ -16,5 +26,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#FBBC05",
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    paddingTop: Platform.OS === "ios" ? 40 : 10,
+    paddingRight: 30,
+    marginTop: 30,
   },
 });
